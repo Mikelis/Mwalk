3581facead53f964ff56ed9419b943e786796ddf
mikeliskaneps <mikelis.kaneps@gmail.com>
2019-07-26 15:05:52 +0300

    experimenting with image scale

--- lib/profile/ProfileWidget.dart
+++ lib/profile/ProfileWidget.dart
@@ -10,14 +10,36 @@ class ProfileWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[new Expanded(child: getBody(data))],
+//    return Column(
+//      mainAxisAlignment: MainAxisAlignment.start,
+//      crossAxisAlignment: CrossAxisAlignment.center,
+//      mainAxisSize: MainAxisSize.max,
+//      children: <Widget>[
+//        getBody(data),
+//        new Text(data.name + " " + data.surname),
+//        new Text(data.description)
+//      ],
+//    );
+    return Container(
+//      constraints: BoxConstraints.expand(width: 300),
+      color: Palette.greenLand,
+      alignment: Alignment.topCenter,
+      child: Image.network(
+        data.image,
+        alignment: Alignment.center,
+        fit: BoxFit.fitWidth,
+        width: 300,
+      ),
     );
   }
 
   Widget getBody(Profile profile) {
     if (profile.image != null) {
-      return new Image.network(profile.image);
+//      return Image.network(
+//        profile.image,
+//        fit: BoxFit.contain,
+//      );
+
     } else {
       return new Builder(
           builder: (context) => FlatButton(
